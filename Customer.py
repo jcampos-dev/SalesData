70b14f54ac5985956cd127d49e4e3ef79d53b1b1
Jim Stewart <jim.stewart@thelangschool.net>
2025-10-17 10:03:58 -0400

    Customer class

---
+++ Customer.py
@@ -0,0 +1,11 @@
+class Customer:
+    def __init__(self,salesperson:str,first:str,last:str,address:str,city:str,state:str,zip:str,phone:str,email:str):
+        self.salesperson:str = salesperson
+        self.first:str = first
+        self.last:str = last
+        self.address:str = address
+        self.city:str = city
+        self.state:str = state
+        self.zip:str = zip
+        self.phone:str = phone
+        self.email:str = email
